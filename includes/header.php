db0fdeb8ccee60ed0fc84c2a92521bcebb47eb78
Luciano Junior <49078083+scofieldjr@users.noreply.github.com>
2019-11-14 01:09:19 -0300

    add includes flies

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Prova Web1</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+      <script src="https://kit.fontawesome.com/1be6ffaefe.js" crossorigin="anonymous"></script>
+
+      <style type="text/css">
+        #TamanhoContainer {
+          width: 700px;
+        }
+      </style>
+    </head>
+
+    <body>
+  <div id="TamanhoContainer" class="container">
+      <h1>Registro de <span class="badge badge-secondary">Filmes</span></h1>
+  </div>
